d275ba242e59801bb5256d3dc5efb985f59220c3
Bharathan Rajaram <bharathan.rajaram@gmail.com>
2009-11-28 17:53:31 +0530

    InputListener started.

--- include/HybridInputListener.h
+++ include/HybridInputListener.h
@@ -7,5 +7,27 @@ namespace HybridRenderer
 {
 	class InputListener: public KeyboardListener, public MouseListener
 	{
+	        private:
+			OIS::InputManager *m_ois;
+	        	OIS::Mouse *mMouse;
+	        	OIS::Keyboard *mKeyboard;
+	        	unsigned long m_hWnd;
+	       		StateManager m_sm;	
+                public:
+	        	InputHandler(StateManager *sim, unsigned long hWnd); 
+        		~InputHandler();
+	
+			void setWindowExtents(int width, int height) ;
+			void capture();
+
+			// MouseListener
+			bool mouseMoved(const OIS::MouseEvent &evt);
+			bool mousePressed(const OIS::MouseEvent &evt, OIS::MouseButtonID);
+			bool mouseReleased(const OIS::MouseEvent &evt, OIS::MouseButtonID);
+	
+			// KeyListener
+			bool keyPressed(const OIS::KeyEvent &evt);
+			bool keyReleased(const OIS::KeyEvent &evt);
+
 	};
 }
