598557dc92579708896ba8c6ecfa16a883d79390
Bharathan Rajaram <bharathan.rajaram@gmail.com>
2009-12-01 22:04:50 +0530

    Got stub headers...

--- src/HybridInputListener.cpp
+++ src/HybridInputListener.cpp
@@ -25,17 +25,6 @@ namespace HybridRenderer
 		//Register to recieve mouse events and keyboard events.
 		mouse->setEventCallback(this);
 		keyboard->setEventCallback(this);
-
-		stateManager = sourceStateManager;
-
-		//Variables for camera movementStepment.
-
-		// set the rotation and movementStep speed
-		rotationStep = 0.13;
-		movementStep = 250;
-
-		cameraMoveDirection = Ogre::Vector3::ZERO;
-
 	}
 
         InputListener::~InputListener() 
@@ -72,84 +61,10 @@ namespace HybridRenderer
 	// KeyListener
 	bool InputListener::keyPressed(const OIS::KeyEvent &evt) 
 	{
-		switch(evt.key)
-		{	
-			case OIS::KC_UP:
-			case OIS::KC_W:
-			    cameraMoveDirection.z = -movementStep;
-			    break;
-		 
-			case OIS::KC_DOWN:
-			case OIS::KC_S:
-			    cameraMoveDirection.z = movementStep;
-			    break;
-		 
-			case OIS::KC_LEFT:
-			case OIS::KC_A:
-			    cameraMoveDirection.x = -movementStep;
-			    break;
-
-			case OIS::KC_RIGHT:
-			case OIS::KC_D:
-			    cameraMoveDirection.x = movementStep;
-			    break;
-
-			case OIS::KC_PGDOWN:
-			case OIS::KC_E:
-			    cameraMoveDirection.y = -movementStep;
-			    break;
-		 
-			case OIS::KC_PGUP:
-			case OIS::KC_Q:
-			    cameraMoveDirection.y = movementStep;
-			    break; 
-
-			case OIS::KC_ESCAPE:
-				stateManager->requestStateChange(SHUTDOWN);
-				break;
-			default:
-				break;
-		}
 		return true;
 	}
 	bool InputListener::keyReleased(const OIS::KeyEvent &evt) 
 	{	
-		switch (evt.key)
-		{
-			case OIS::KC_UP:
-			case OIS::KC_W:
-		    	 	cameraMoveDirection.z = 0;
-		  	  	break;
-	 
-			case OIS::KC_DOWN:
-			case OIS::KC_S:
-			    cameraMoveDirection.z = 0;
-			    break;
-	 
-			case OIS::KC_LEFT:
-			case OIS::KC_A:
-			    cameraMoveDirection.x = 0;
-			    break;
-	 
-			case OIS::KC_RIGHT:
-			case OIS::KC_D:
-			    cameraMoveDirection.x = 0;
-			    break;
-	 	
-			case OIS::KC_PGDOWN:
-			case OIS::KC_E:
-			    cameraMoveDirection.y = 0;
-			    break;
-	 
-			case OIS::KC_PGUP:
-			case OIS::KC_Q:
-			    cameraMoveDirection.y = 0;
-			    break;
-	 
-			default:
-			    break;
-		} // switch
-
 		return true;
 	}
 
