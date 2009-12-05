f7c1f084644f3bedb062e2c3b2a133fdaad4ff79
Bharathan Rajaram <bharathan.rajaram@gmail.com>
2009-12-05 14:38:34 +0530

    I've fixed the errors. Input system is not causing anything to happen!

--- src/HybridFrameListener.cpp
+++ src/HybridFrameListener.cpp
@@ -22,8 +22,8 @@ namespace HybridRenderer
 
 	bool FrameListener::frameStarted(const Ogre::FrameEvent& evt)
 	{
-		std::pair<Ogre::Vector3, Ogre::Quaternion> result =  camInputHandler->getTransformations();
-		Ogre::Vector3 camMove = result.first;
+		std::pair<Ogre::Vector3*, Ogre::Quaternion*> result =  camInputHandler->getTransformations();
+		Ogre::Vector3 camMove = *(result.first);
 		camNode->translate(camMove,Ogre::Node::TS_LOCAL);
 		return true;
 	}
